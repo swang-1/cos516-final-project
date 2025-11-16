d5a720728d459d67d60591354d1fd6a8abdd5b48
swang-1 <swang6987@gmail.com>
2025-11-16 18:13:45 -0500

    Add protocols and define Lean project structure

--- InvariantSearch.lean
+++ InvariantSearch.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `VeilUsageExample` library.
 -- Import modules here that should be built as part of the library.
-import VeilUsageExample.Ring
+import VeilProtocols.Ring
+import VeilProtocols.TwoPhaseCommit
---
+++ VeilProtocols/Ring.lean
@@ -0,0 +1,52 @@
+import Veil
+
+-- From https://github.com/verse-lab/veil/blob/main/Examples/IvyBench/Ring.lean
+
+veil module Ring
+
+
+type node
+instantiate tot : TotalOrder node
+instantiate btwn : Between node
+
+
+open Between TotalOrder
+
+relation leader : node -> Prop
+relation pending : node -> node -> Prop
+
+#gen_state
+
+after_init {
+  leader N := False
+  pending M N := False
+}
+
+action send (n next : node) = {
+  require ∀ Z, n ≠ next ∧ ((Z ≠ n ∧ Z ≠ next) → btw n next Z)
+  pending n next := True
+}
+
+action recv (sender n next : node) = {
+  require ∀ Z, n ≠ next ∧ ((Z ≠ n ∧ Z ≠ next) → btw n next Z)
+  require pending sender n
+  -- message may or may not be removed
+  -- this models that multiple messages might be in flight
+  pending sender n := *
+  if (sender = n) then
+    leader n := True
+  else
+    -- pass message to next node
+    if (le n sender) then
+      pending sender next := True
+}
+
+safety [single_leader] leader L → le N L
+invariant pending S D ∧ btw S N D → le N S
+invariant pending L L → le N L
+
+#gen_spec
+
+#check_invariants
+
+end Ring
